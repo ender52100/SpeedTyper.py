0d5e38610bbe1fcc597628525b72578dd1806d07
ender52100 <49796401+ender52100@users.noreply.github.com>
2025-11-28 22:07:47 +0100

    Implement resource_path function for resource handling
    Add resource_path function to handle resource paths for development and PyInstaller.

--- speed_typer.py
+++ speed_typer.py
@@ -1,3 +1,16 @@
+import os
+import sys
+
+def resource_path(relative_path):
+    """ Obtient le chemin absolu vers la ressource, fonctionne pour dev et pour PyInstaller """
+    try:
+        # PyInstaller crée un dossier temporaire et stocke le chemin dans _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 #!/usr/bin/env python3
 # speed_typer.py
 """
